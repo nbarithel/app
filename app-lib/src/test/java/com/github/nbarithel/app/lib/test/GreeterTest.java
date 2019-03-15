c1a520d90b7b85532daf6e6c92162d01bba7e48c
Nicolas Barithel <nbarithel@gmail.com>
2019-03-15 11:20:36 +0100

    Update JUnit 5.4.0

--- app-lib/src/test/java/com/github/nbarithel/app/lib/test/GreeterTest.java
+++ app-lib/src/test/java/com/github/nbarithel/app/lib/test/GreeterTest.java
@@ -1,16 +1,13 @@
 package com.github.nbarithel.app.lib.test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
 
 import com.github.nbarithel.app.lib.Greeter;
 
-@RunWith(JUnitPlatform.class)
 public class GreeterTest {
 
   private static Greeter greeter;
